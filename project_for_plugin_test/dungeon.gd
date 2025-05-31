704821a9c85de56340f1d0f437d915c7757610a8
420Pierre <127946382+420Pierre@users.noreply.github.com>
2025-05-31 17:43:25 +0200

    Added Hallways
    Made the hallways and the connection points visible.

--- project_for_plugin_test/dungeon.gd
+++ project_for_plugin_test/dungeon.gd
@@ -1,40 +1,41 @@
-@tool 
+@tool
 extends Node3D
-@onready var grid_map : GridMap = $GridMap
 
+@onready var grid_map : GridMap = $GridMap
 @export var start : bool = false : set = set_start
-func set_start(val:bool)->void:
+
+func set_start(val: bool) -> void:
 	if Engine.is_editor_hint():
 		generate()
 
 @export_range(0, 1) var chance : float = 0.25
 @export var border_size : int = 20 : set = set_border_size
-func set_border_size(val : int)->void:
+func set_border_size(val : int) -> void:
 	border_size = val
 	if Engine.is_editor_hint():
 		show_border()
 
-@export var room_number : int = 5	
-@export var min_room_size : int = 2
-@export var max_room_size : int = 4
+@export var room_number : int = 4
 @export var room_margin : int = 1 #Mindestanzahl, damit Räume nicht überlappen
 @export var room_recursion : int = 15 #ansonsten gibt es stack overflow bei zu vielen Räumen
-@export_multiline var custom_seed : String = "" : set = set_seed
+@export var min_room_size : int = 2 
+@export var max_room_size : int = 4
+@export_multiline var custom_seed : String = "" : set = set_seed 
 func set_seed(val:String)->void:
 	custom_seed = val
 	seed(val.hash())
 
 var room_tiles : Array[PackedVector3Array] = []
 var room_positions : PackedVector3Array = []
-	
+
 func show_border():
 	grid_map.clear()
-	for i in range (-1, border_size+1):
-		grid_map.set_cell_item(Vector3i(i,0,-1),3)
-		grid_map.set_cell_item(Vector3i(i,0,border_size),3)
-		grid_map.set_cell_item(Vector3i(border_size,0,i),3)
-		grid_map.set_cell_item(Vector3i(-1,0,i),3)
-	
+	for i in range(-1, border_size + 1):
+		grid_map.set_cell_item(Vector3i(i, 0, -1), 3)
+		grid_map.set_cell_item(Vector3i(i, 0, border_size), 3)
+		grid_map.set_cell_item(Vector3i(border_size, 0, i), 3)
+		grid_map.set_cell_item(Vector3i(-1, 0, i), 3)
+
 func generate():
 	room_tiles.clear()
 	room_positions.clear()
@@ -42,29 +43,27 @@ func generate():
 	if custom_seed : set_seed(custom_seed)
 	show_border()
 	for i in room_number:
-		t+=1
 		generate_room(room_recursion)
-		if t%17 == 16: await get_tree().create_timer(0).timeout
 	
 	var rpv2 : PackedVector2Array = []
 	var del_graph : AStar2D = AStar2D.new()
 	var mst_graph : AStar2D = AStar2D.new()
 	
 	for p in room_positions:
-		rpv2.append(Vector2(p.x,p.z))
-		del_graph.add_point(del_graph.get_available_point_id(), Vector2(p.x,p.z))
-		mst_graph.add_point(mst_graph.get_available_point_id(), Vector2(p.x,p.z)) #punkte bisher nur platziert, sind aber noch nicht verbunden
-		
+		rpv2.append(Vector2(p.x, p.z))
+		del_graph.add_point(del_graph.get_available_point_id(), Vector2(p.x, p.z))
+		mst_graph.add_point(mst_graph.get_available_point_id(), Vector2(p.x, p.z)) #punkte bisher nur platziert, sind aber noch nicht verbunden
+	
 	var delaunay : Array = Array(Geometry2D.triangulate_delaunay(rpv2))
-		
-	for i in delaunay.size()/3: #einfach hardcoden, weil es in einem dreieck nur drei punkte gibt
+	
+	for i in delaunay.size() / 3: #einfach hardcoden, weil es in einem dreieck nur drei punkte gibt
 		var p1 : int = delaunay.pop_front()
 		var p2 : int = delaunay.pop_front()
 		var p3 : int = delaunay.pop_front()
-		del_graph.connect_points(p1,p2)
-		del_graph.connect_points(p2,p3)
-		del_graph.connect_points(p1,p3)
-			
+		del_graph.connect_points(p1, p2)
+		del_graph.connect_points(p2, p3)
+		del_graph.connect_points(p1, p3)
+	
 	var visited_points : PackedInt32Array = []
 	visited_points.append(randi() % room_positions.size())
 	while visited_points.size() != mst_graph.get_point_count():
@@ -74,50 +73,55 @@ func generate():
 				if !visited_points.has(c):
 					var con : PackedInt32Array = [vp, c]
 					possible_connections.append(con)
-						
+					
 		var connection : PackedInt32Array = possible_connections.pick_random()
 		for pc in possible_connections:
 			if rpv2[pc[0]].distance_squared_to(rpv2[pc[1]]) <\
 			rpv2[connection[0]].distance_squared_to(rpv2[connection[1]]):
-				connection = pc 
-					
+				connection = pc
+		
 		visited_points.append(connection[1])
 		mst_graph.connect_points(connection[0], connection[1])
 		del_graph.disconnect_points(connection[0], connection[1])
-			
+	
 	var hallway_graph : AStar2D = mst_graph
-		
+	
 	for p in del_graph.get_point_ids():
 		for c in del_graph.get_point_connections(p):
-			if c>p:
+			if c > p:
 				var kill : float = randf()
-				if chance > kill :
+				if chance > kill:
 					hallway_graph.connect_points(p, c)
-					
 	generate_hallways(hallway_graph)
-	
-func generate_hallways(hallway_graph:AStar2D):
+
+func generate_hallways(hallway_graph : AStar2D):
 	var hallways : Array[PackedVector3Array] = []
-	for p in hallway_graph.get_available_point_ids():
+	for p in hallway_graph.get_point_ids():
 		for c in hallway_graph.get_point_connections(p):
-			if c>p:
+			if c > p:
 				var room_from : PackedVector3Array = room_tiles[p]
 				var room_to : PackedVector3Array = room_tiles[c]
 				var tile_from : Vector3 = room_from[0]
 				var tile_to : Vector3 = room_to[0]
+				
 				for t in room_from:
-					if t.distance_squared_to(room_positions[c])<\
+					if t.distance_squared_to(room_positions[c]) <\
 					tile_from.distance_squared_to(room_positions[c]):
 						tile_from = t
 				for t in room_to:
-					if t.distance_squared_to(room_positions[p])<\
+					if t.distance_squared_to(room_positions[p]) <\
 					tile_to.distance_squared_to(room_positions[p]):
 						tile_to = t
+						
 				var hallway : PackedVector3Array = [tile_from, tile_to]
 				hallways.append(hallway)
-				grid_map.set_cell_item(tile_from, 2)
-				grid_map.set_cell_item(tile_to, 2)
-	
+
+				# Tür-Positionen korrekt setzen (wichtig: Vector3 → Vector3i!)
+				var v3_from = Vector3i(tile_from.x, 0, tile_from.z)
+				var v3_to = Vector3i(tile_to.x, 0, tile_to.z)
+				grid_map.set_cell_item(v3_from, 2)
+				grid_map.set_cell_item(v3_to, 2)
+
 	var astar : AStarGrid2D = AStarGrid2D.new()
 	astar.size = Vector2i.ONE * border_size
 	astar.update()
@@ -125,47 +129,46 @@ func generate_hallways(hallway_graph:AStar2D):
 	astar.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
 	
 	for t in grid_map.get_used_cells_by_item(0):
-		astar.set_point_solid(Vector2i(t.x,t.z))
-		
+		astar.set_point_solid(Vector2i(t.x, t.z))
+
 	for h in hallways:
 		var pos_from : Vector2i = Vector2i(h[0].x, h[0].z)
 		var pos_to : Vector2i = Vector2i(h[1].x, h[1].z)
 		var hall : PackedVector2Array = astar.get_point_path(pos_from, pos_to)
 		
 		for t in hall:
-			var pos: Vector3i = Vector3i(t.x,0,t.y)
+			var pos: Vector3i = Vector3i(t.x, 0, t.y)
 			if grid_map.get_cell_item(pos) < 0:
-				grid_map.set_cell_item(pos, 1)
-	
-func generate_room(rec:int):
-	
-	if !rec>0:
+				grid_map.set_cell_item(pos, 1) # setze Hallway-Tile
+
+func generate_room(rec: int):
+	if !rec > 0:
 		return
 	
 	var width : int = (randi() % (max_room_size - min_room_size)) + min_room_size
 	var height : int = (randi() % (max_room_size - min_room_size)) + min_room_size
 	
-	var starting_position : Vector3i
-	starting_position.x = randi() % (border_size - width + 1) #+1 nötig, weil nie gleich border sein kann
-	starting_position.z = randi() % (border_size - height + 1)
+	var start_pos : Vector3i 
+	start_pos.x = randi() % (border_size - width + 1) #+1 nötig, weil nie gleich border sein kann
+	start_pos.z = randi() % (border_size - height + 1)
 	
 	for r in range(-room_margin, height + room_margin): #damit Räume nicht überlappen
 		for c in range(-room_margin, width + room_margin):
-			var pos : Vector3i = starting_position + Vector3i(c,0,r)
+			var pos : Vector3i = start_pos + Vector3i(c, 0, r)
 			if grid_map.get_cell_item(pos) == 0:
-				generate_room(rec-1)
+				generate_room(rec - 1)
 				return
 	
 	var room : PackedVector3Array = []
 	for r in height:
 		for c in width:
-			var pos : Vector3i = starting_position + Vector3i(c,0,r)
-			grid_map.set_cell_item(pos,0)
+			var pos : Vector3i = start_pos + Vector3i(c, 0, r)
+			grid_map.set_cell_item(pos, 0)
 			room.append(pos)
+
 	room_tiles.append(room)
-	var avg_x : float = starting_position.x + (float(width)/2)
-	var avg_z : float = starting_position.z + (float(height)/2)
+	
+	var avg_x : float = start_pos.x + (float(width) / 2)
+	var avg_z : float = start_pos.z + (float(height) / 2)
 	var pos : Vector3 = Vector3(avg_x, 0, avg_z)
 	room_positions.append(pos) #machen zu Vector3 und fügen es dann oben zum Array hinzu
-	
-	#pass
